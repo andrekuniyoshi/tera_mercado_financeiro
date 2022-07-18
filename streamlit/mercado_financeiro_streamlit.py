e651039dbe196246b346a3ae4f59c776ceddc23a
andrekuniyoshi <81191834+andrekuniyoshi@users.noreply.github.com>
2022-07-18 13:47:15 -0300

    Update mercado_financeiro_streamlit.py

--- streamlit/mercado_financeiro_streamlit.py
+++ streamlit/mercado_financeiro_streamlit.py
@@ -296,7 +296,7 @@ if st.button('Aperte para Previsão'):
 #st.dataframe(df)
 
 ###-----------------------------------MODELO--------------------------------------------
-        picklefile = open("emp-model.pkl", "rb")
+        picklefile = open("stock_pred3.pkl", "rb")
         model = pickle.load(picklefile)
 	
         df = df_model[-600:]
